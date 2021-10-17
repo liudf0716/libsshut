09e54a0df4732a480a9f39142b6cd38838e6f9f5
Dengfeng Liu <liudf0716@gmail.com>
2021-10-17 16:38:25 +0800

    Update sshut_debug.c

--- sshut_debug.c
+++ sshut_debug.c
@@ -3,17 +3,91 @@
 #include <event.h>
 #include "sshut.h"
 
+#define htonll(x) ((1==htonl(1)) ? (x) : ((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+#define ntohll(x) htonll(x)
 
-static void
-_cb_exec(struct sshut_action *action, enum sshut_error error, char *cmd, char *output, int output_len, void *arg)
+static void _ws_request(bufferevent* );
+static void _ws_2_ssh(struct evbuffer* , struct sshut* );
+static void _cb_ws_recv(bufferevent* bev, void* ptr);
+
+struct evdns_base 	*dns_base;
+struct bufferevent 	*b_ws;
+
+const char* fixed_key = "dGhlIHNhbXBsZSBub25jZQ==";
+const char* fixed_accept = "s3pPLMBiTxaQ9kYGzzhZRbK+xOo=";
+const char* uri = "/ws/sshproxy";
+const char* ws_host = "106.13.23.50";
+const short ws_port = 8001;
+
+static void 
+_ws_2_ssh(struct evbuffer* buf, struct sshut* ssh)
 {
-	if (error != SSHUT_NOERROR)
-		sshut_err_print(error);
-	else {
-		printf("> %s\n", cmd);
-		printf("%s\n", output);
+	size_t data_len = evbuffer_get_length(buf);
+	printf("receive data_len %d\n", data_len);
+	if(data_len < 2)
+		return;
+
+	unsigned char* data = evbuffer_pullup(buf, data_len);
+
+	int fin = !!(*data & 0x80);
+	int opcode = *data & 0x0F;
+	int mask = !!(*(data+1) & 0x80);
+	uint64_t payload_len =  *(data+1) & 0x7F;
+
+	size_t header_len = 2 + (mask ? 4 : 0);
+	
+	if(payload_len < 126){
+		if(header_len > data_len)
+			return;
+	}else if(payload_len == 126){
+		header_len += 2;
+		if(header_len > data_len)
+			return;
+		payload_len = ntohs(*(uint16_t*)(data+2));
+	}else if(payload_len == 127){
+		header_len += 8;
+		if(header_len > data_len)
+			return;
+		payload_len = ntohll(*(uint64_t*)(data+2));
 	}
-	event_base_loopbreak(action->ssh->evb);
+
+	if(header_len + payload_len > data_len)
+		return;
+
+	const unsigned char* mask_key = data + header_len - 4;
+	printf("data_len %d mask %x head_len %d payload_len %d opcode %x\n",
+		  data_len, mask, header_len, payload_len, opcode);
+	for(int i = 0; mask && i < payload_len; i++)
+		data[header_len + i] ^= mask_key[i%4];
+
+	if(opcode == 0x01) {
+		printf("receive ws text data\n");
+		
+	} else if (opcode == 0x02) {
+		printf("receive ws bin data\n");
+		
+	}else if(!fin){
+		printf("frame to be continue...\n");
+	}
+
+	evbuffer_drain(buf, header_len + payload_len);
+
+	//next frame
+	_ws_2_ssh(buf, ssh);
+}
+
+static void 
+_ws_request(bufferevent* bev){
+	struct evbuffer *out = bufferevent_get_output(bev);
+	evbuffer_add_printf(out, "GET %s HTTP/1.1\r\n", uri);
+	evbuffer_add_printf(out, "Host:%s:%d\r\n",host, port);
+	evbuffer_add_printf(out, "Upgrade:websocket\r\n");
+	evbuffer_add_printf(out, "Connection:upgrade\r\n");
+	evbuffer_add_printf(out, "Sec-WebSocket-Key:%s\r\n", fixed_key);
+	evbuffer_add_printf(out, "Sec-WebSocket-Version:13\r\n");
+	evbuffer_add_printf(out, "Origin:http://%s:%d\r\n",host, port); //missing this key will lead to 403 response.
+
+	evbuffer_add_printf(out, "\r\n");
 }
 
 static void
@@ -55,6 +129,47 @@ _cb_disconnect(struct sshut *ssh, enum sshut_error error, void *arg)
 	event_base_loopbreak(ssh->evb);
 }
 
+static void 
+_cb_ws_recv(bufferevent* bev, void* ptr)
+{
+	static bool upgraded = false;
+	struct evbuffer *input = bufferevent_get_input(bev);
+	
+	if(!upgraded){
+		int data_len = evbuffer_get_length(input);
+		unsigned char* data = evbuffer_pullup(input, data_len);
+		if(!strstr((const char*)data, "\r\n\r\n"))
+			return;
+		if(strncmp((const char*)data, "HTTP/1.1 101", strlen("HTTP/1.1 101")) != 0
+				|| !strstr((const char*)data, fixed_accept)){
+			printf("ws upgraded failed\n");
+		}else{
+			//drain 
+			evbuffer_drain(input, data_len);
+			upgraded = true;
+			printf("ws upgraded success\n");
+		}
+	}else{
+		if (!ptr) {
+			return;
+		}
+		struct sshut *ssh = ptr;
+		printf("forward ws frame to ssh server\n");
+		ws_2_ssh(input, ssh);
+	}
+}
+
+static void 
+_cb_ws_event(bufferevent* bev, short events, void* ptr)
+{
+	if(events & BEV_EVENT_CONNECTED){
+		printf("ws connected\n");
+		_ws_request(bev);
+	}else{
+		printf("ws disconnected\n");
+	}
+}
+
 int
 main(int argc, char **argv)
 {
@@ -62,17 +177,25 @@ main(int argc, char **argv)
 	struct sshut_auth *auth;
 	struct sshut *ssh;
 	
-	if (argc != 4) {
-		printf("sshut_debug user password port\n");
+	if (argc != 6) {
+		printf("sshut_debug user password port wshost wsport\n");
 		return 0;
 	}
 	
 	evb = event_base_new();
+	dns_base = evdns_base_new(evb, 1);
 
 	auth = sshut_auth_new();
 	sshut_auth_add_userpass(auth, argv[1], argv[2]);
 	ssh = sshut_new(evb, "127.0.0.1", atoi(argv[3]), auth, SSHUT_NORECONNECT, SSHUT_NOVERBOSE,
 		_cb_connect, _cb_disconnect, NULL);
+	b_ws = bufferevent_socket_new(evb, -1, BEV_OPT_CLOSE_ON_FREE);
+	ssh->conn.b_ws = b_ws;
+	
+	bufferevent_setcb(b_ws, _cb_ws_recv, NULL, _cb_ws_event, ssh);
+	bufferevent_enable(b_ws, EV_READ|EV_WRITE);
+	bufferevent_socket_connect_hostname(b_ws, dns_base, AF_INET, argv[4], atoi(argv[5]));
+	
 	event_base_dispatch(evb);
 
 	sshut_auth_free(auth);
